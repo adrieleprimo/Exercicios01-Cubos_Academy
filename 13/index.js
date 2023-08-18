04f14eb849f609d737a329b87fb3aaa5af245bd9
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 17:22:45 -0300

    Area total do cilindro

---
+++ 13/index.js
@@ -0,0 +1,8 @@
+console.log("-------------------------");
+console.log("Área total de um cilindro")
+console.log("-------------------------");
+let R = 3;
+let H = 1;
+let At =2*R*(R+H);
+
+console.log(`O valor da área total de um cilindro é ${At}pi`);
