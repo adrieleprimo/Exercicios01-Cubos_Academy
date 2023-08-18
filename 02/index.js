362321d4874d4977dea060ddc1b63233a4895d5d
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 12:49:04 -0300

    Conversão de Farenheit para Celsius

---
+++ 02/index.js
@@ -0,0 +1,7 @@
+console.log("------------------------------------------------");
+console.log("Bem-vindo ao conversor de Farenheit para Celsius");
+console.log("------------------------------------------------");
+let F = 73;
+let C = 5/9 *(F-32);
+console.log(`O valor em celsius é ${C.toFixed(0)}`);
+console.log("------------------------------------------------");
