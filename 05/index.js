99ad0b32f227d14b650d01652b0fe17ff506d056
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 13:19:13 -0300

    Calculo entre duas distâncias

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,10 @@
+console.log("-------------------------------------");
+console.log("Calcule a distância entre dois pontos")
+console.log("-------------------------------------");
+let x1 = 1;
+let x2 = 1;
+let y1 = 1;
+let y2 = 4;
 
+let distancia = Math.pow((x1 - x2), 2) + Math.pow((y2 - y1), 2 );
+console.log(`A distância entre os pontos X e Y é ${Math.sqrt(distancia)} `);
