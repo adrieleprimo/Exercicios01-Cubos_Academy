d08ad773b956a42dbfb25e4f43fb44713eed78a5
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 13:44:58 -0300

    Calculando do perímetro e área do Círculo

---
+++ 08/index.js
@@ -0,0 +1,6 @@
+console.log("-------------------------------------------");
+console.log("calculando o perímetro e área de um circulo");
+console.log("-------------------------------------------");
+let C = 2* Math.PI * 1;
+let A = Math.PI * Math.sqrt(1);
+console.log(`O valor do comprimento é ${C.toFixed(1)} e da área é ${A.toFixed(2)}`);
