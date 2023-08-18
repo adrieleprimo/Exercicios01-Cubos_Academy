d58112cc0a9e37bcc1f41b81782d48150fe7a550
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 13:40:02 -0300

    Calculo de infectados pela COVID-19

---
+++ 07/index.js
@@ -0,0 +1,8 @@
+console.log("-----------------");
+console.log("Casos de COVID-19");
+console.log("-----------------")
+let Po = 1000;
+let x = 4;
+let t = 7;
+let P = Po * x ;
+console.log(`A quantidade de pessoas infectadas é de ${P} pessoas`)
