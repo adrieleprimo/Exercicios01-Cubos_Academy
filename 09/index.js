7cc9b732ef0485e646bf24292313d8a597e923ab
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 13:49:24 -0300

    Soma dos ângulos do polígono

---
+++ 09/index.js
@@ -0,0 +1,6 @@
+console.log("----------------------------");
+console.log("Soma dos ângulos do polígono");
+console.log("----------------------------");
+let  S = 720;
+let A = S/6;
+console.log(`O valor da soma dos ângulos internos é ${S} e o valor de cada ângulo é ${A}`);
