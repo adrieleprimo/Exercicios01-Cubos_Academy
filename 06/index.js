286908d35077bb6674a60238fb756cb47d886726
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 13:23:39 -0300

    Calculo da velocidade média

---
+++ 06/index.js
@@ -0,0 +1,7 @@
+console.log("---------------------------");
+console.log("Calculo da velocidade média");
+console.log("---------------------------");
+let distanciaPercorrida = 500;
+let tempo = 10;
+let velocMedia = distanciaPercorrida/tempo *3.6;
+console.log(`A velocidade média é ${velocMedia}km/h`)
