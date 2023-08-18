a769dce78cbdafef0adf2207d90fe5b6c84ed99f
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 14:46:00 -0300

    O valor do volume da esfera

---
+++ 11/index.js
@@ -0,0 +1,5 @@
+console.log("-----------------------------------------");
+console.log("Qual o volume de uma determinada esfera? ");
+console.log("-----------------------------------------");
+let volume = 4/3 * Math.pow(3,3);
+console.log(`O volume da esfera é ${volume}pi`);
