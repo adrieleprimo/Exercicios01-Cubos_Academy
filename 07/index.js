005a94f12745fb8fae29a5401c1489926ad36700
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-18 17:06:18 -0300

    Exercicio de Casos de Covid organizado e Taxas de juros

--- 07/index.js
+++ 07/index.js
@@ -2,7 +2,7 @@ console.log("-----------------");
 console.log("Casos de COVID-19");
 console.log("-----------------")
 let Po = 1000;
-let x = 4;
-let t = 7;
-let P = Po * x ;
+let taxa = 4;
+let dias = 7;
+let P = Po * Math.pow(taxa, dias/dias);
 console.log(`A quantidade de pessoas infectadas é de ${P} pessoas`)
---
+++ 12/index.js
@@ -0,0 +1,9 @@
+console.log("--------------");
+console.log("Taxas de Juros");
+console.log("--------------");
+let M = 90000;
+let C = 60000;
+let N = 24;
+let i =((Math.pow(M/C, 1/N)-1)*100).toFixed(3);
+
+console.log(`A taxa de juros é ${i} `)
